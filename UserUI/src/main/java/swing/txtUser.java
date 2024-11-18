ac332020c61d52efe5c1948d6be3b00c83b91694
tin75vn@gmail.com <vttin22@clc.fitus.edu.vn>
2024-11-18 19:48:54 +0700

    add UserUI

---
+++ UserUI/src/main/java/swing/txtUser.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package swing;
+
+
+public class txtUser {
+
+    public static void grabFocus() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
